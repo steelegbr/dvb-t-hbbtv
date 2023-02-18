99225d30c28cb4058b6cba59329f58f9ebab30d5
Marc Steele <steelegbr@gmail.com>
2023-02-18 15:51:02 +0000

    Add extra logging

--- Payloads/001-Logger/index.js
+++ Payloads/001-Logger/index.js
@@ -3,6 +3,7 @@ window.addEventListener("load", init);
 var nav;
 
 function init() {
+  console.log("hbbtv");
   var appManager = document.getElementById("applicationManager");
   var appObject = appManager.getOwnerApplication(document);
   if (appObject) {
