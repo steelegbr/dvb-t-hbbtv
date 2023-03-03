9d151cb1dd6241aabb8da334204cc7626d7fba4d
Marc Steele <steelegbr@gmail.com>
2023-03-03 19:26:30 +0000

    Trying to add a bit of magic

--- Payloads/003-Temp/index.js
+++ Payloads/003-Temp/index.js
@@ -1,9 +1,14 @@
 window.addEventListener("load", init);
 
 function init() {
-    console.log("Here!");
-    var logger = new Logger("002-PortScanner");
-    logger.log("Test 123!");
-    Logger.show();
+    var appManager = document.getElementById("applicationManager");
+    var appObject = appManager.getOwnerApplication(document);
+    if (appObject) {
+        appObject.show();
+        console.log("Here!");
+        var logger = new Logger("002-PortScanner");
+        logger.log("Test 123!");
+        Logger.show();
+    }
 }
 
